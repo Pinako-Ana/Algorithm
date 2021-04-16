6a63ae2cf17008468881e1f477e392145d651d59
Pinako <BloodyNose29@gmail.com>
2021-04-16 18:05:59 +0900

    algorithm

--- BaekJoonCpp/BJT_2580.cpp
+++ BaekJoonCpp/BJT_2580.cpp
@@ -1,62 +1,47 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
-int cnt;
-vector <pair < pair<int, int>,int > > v;
-int s[10][10];
+int cnt = 81;
+int s[9][9];
 
-bool hzt(int horizontal, int num){
-	find()
-	
-	return false;
+bool check(int h, int v, int num){//수평,수직,9칸 검사를함
+	for (int i = 0; i < 9; i++){
+		if(s[h][i] == num || s[i][v] == num) return false;
+	}
+	int x = (h/3) * 3;
+	int y = (v/3) * 3;;
+	for (int i = x; i <= x+2; i++){
+		for (int j = y; j <= y+2; j++){
+			if(s[i][j] == num) return false;}
+	}return true;//세 조건들에서 안걸려야 true반환해줌
 }
-
-bool vtc(int vertical, int num){
-	return false;
-}
-
-bool nine(int hor, int ver, int num){
-	return false;
-}
-
-
-
 void dfs(int time){
-	if(time == cnt+1){
-		for (int i = 1; i < 10; i++){
-			for (int l = 1; l < 10; l++){
-				cout << s[i][l] << ' ';
-			}
+	int j =	time/9;
+	int k = time%9;
+	if(time == cnt){
+		for (int i = 0; i < 9; i++){
+			for (int p = 0; p < 9; p++){cout << s[i][p] << ' ';}
 			cout << '\n';
 		}
-		return;
+		exit(0);
 	}
-	for (int j = 1; j <= 9; j++){
-		for (int k = 1; k <= 9; k++){
-			for (int l = 1; l <= 9; l++){
-				if(s[j][k] == 0 && hzt(j,l) && vtc(k,l) && nine(j,k,l)){
-					s[j][k] = l;
-					dfs(time++);
-					s[j][k] = 0;
-				}
-			}
+	if(s[j][k] == 0){
+		for (int l = 1; l <= 9; l++){
+			if(check(j,k,l)){//이게 참이면 숫자 넣어도 된다
+				s[j][k] = l;
+				dfs(time + 1);
+				s[j][k] = 0; //다시 0으로 해줘야함
+			}	
 		}
-	}
+	}else dfs(time + 1);
 }
-
 int main(void){
+	ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 	for (int i = 0; i < 9; i++){
-		for (int l = 0; l < 9; l++){
-			cin >> s[i][l];
-			if(s[i][l] == 0)
-				cnt++;
-		}
+		for (int l = 0; l < 9; l++){cin >> s[i][l];}
 	}
-	
-
-	dfs(1);
-
+	dfs(0);
 	return 0;
 }
