67cc0cffcb525d493080c5a86ae9a4fa90d23620
Pinako <BloodyNose29@gmail.com>
2021-04-18 20:10:54 +0900

    algorithm

--- BaekJoonCpp/BJT_14889.cpp
+++ BaekJoonCpp/BJT_14889.cpp
@@ -7,20 +7,22 @@ using namespace std;
 
 int n;
 int arr[MAX][MAX];
-vector <int> starteam;
-vector <int> linkteam;
-int list[MAX];
-vector <int> pairlist;
 
-int list2[MAX];
-vector <int> couple;
+//1이들어간 팀 안들어간팀 이렇게 구분했다 (반으로 나눠지는것이므로)
+vector <int> starteam; //1이 들어가는 팀의 조합마다의 Sij의 합
+vector <int> linkteam; 
+
+int list[MAX]; //n/2 만큼의 조합들 구할때 쓰는 배열
+vector <int> pairlist; // n/2길이의 조합 
+
+int list2[MAX]; //Sij구하려고 만든 배열
+vector <int> couple;// n/2길이의 조합을 가지고 또 거기서 2개씩 뽑는 조합을 만듬
 
 int sum = 0 ;
 void cp(int x){
-	
 	if(x > 2){
 		sum += arr[couple[1]][couple[2]] + arr[couple[2]][couple[1]];
-		return;
+		return;// sum에는 n/2 길이의 조합 당 Sij의 값들이 추가됨
 	}
 	for (int k = 1; k <= n/2; k++){
 		if(list2[k] == 0 && couple.back() < pairlist[k]){
@@ -35,11 +37,11 @@ void cp(int x){
 void dfs(int times){
 	if(times > n/2){
 		cp(1);
-		if(pairlist[1] == 1)
+		if(pairlist[1] == 1)//1을 기준으로 나눠서 담을 것이므로
 			starteam.push_back(sum);
-		else
+		else 
 			linkteam.push_back(sum);
-		sum = 0;
+		sum = 0; //꼭 초기화해줘야함
 		return;
 	}
 	for (int i = 1; i <= n; i++){
@@ -59,17 +61,13 @@ int main(void){
 	pairlist.push_back(-1);
 	couple.push_back(-1);
 	dfs(1);
-	reverse(linkteam.begin(),linkteam.end());
+	reverse(linkteam.begin(),linkteam.end());//123일때 456이다 즉 링크팀을 리버스 해줘야 서로의 팀간의 능력 차이를 구할 수 있다
 	int minimum = 10000000;
-	
 	for (int j = 0; j < starteam.size(); j++){
-		if(minimum > abs(starteam[j] -linkteam[j]))
-			minimum = abs(starteam[j] -linkteam[j]);
+		int a = abs(starteam[j] -linkteam[j]);
+		if(minimum > a)
+			minimum = a;
 	}
 	cout << minimum;
-	
-	
-
-	
 	return 0;
 }
